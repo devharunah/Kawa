eeef6d79e908c1e7b01460575242a8db78ce9e89
devharunah <kakoozaharunah93@gmail.com>
2025-12-11 14:07:42 +0300

    Inizialized project & colour schemear

---
+++ app/src/main/java/com/example/kawa/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.kawa.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val black = Color(0)
+val green = Color(0xFF075B07)
+val green_light = Color(0xFFb2e5b2)
+val light_colour = Color(0xFFffeadb)
+
